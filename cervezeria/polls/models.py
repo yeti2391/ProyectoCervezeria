a210fad37df7370d2f8fbfdbb962fcd23cec2a3c
Yeti <marco2391@gmail.com>
2020-05-18 22:40:50 -0300

    arreglos minimos en el modelo. Sigue sin funcionar agregar Marca

--- cervezeria/polls/models.py
+++ cervezeria/polls/models.py
@@ -4,38 +4,41 @@ from django.db import models
 
 class TipoCerveza(models.Model):
     id = models.AutoField(primary_key=True)
-    tipo=models.CharField(max_length=30)
+    tipo=models.CharField(max_length=30, blank=False, null= False )
 
     class Meta:
         ordering=["tipo"]
+        verbose_name= 'Tipo'
         verbose_name_plural = "Tipos"
 
     def __str__(self):
         return self.tipo
 
-class NombreCerveza(models.Model):
-    tipo=models.ForeignKey(TipoCerveza, null=True, on_delete=models.CASCADE)
-    #marca=models.ForeignKey(MarcaCerveza, null=True, on_delete=models.CASCADE)
-    nombre=models.CharField(max_length=30)
-    color=models.CharField(max_length=30)
-    descripcion=models.CharField(max_length=300)
-
-    class Meta:
-        ordering=["nombre"]
-        verbose_name_plural = "Nombres"
-
-    def __str__(self):
-        return "%s %s %s %s" % (self.tipo, self.nombre, self.color, self.descripcion)
-
 class MarcaCerveza(models.Model):
     id = models.AutoField(primary_key=True)
-    marca=models.CharField(max_length=50)
+    marca=models.CharField(max_length=50, blank=True, null= True)
     pais=models.CharField(max_length=50)
     sitioweb=models.URLField(blank=True, null=True)
 
     class Meta:
         ordering=["marca"]
+        verbose_name= 'marca'
         verbose_name_plural = "marcas"
 
     def __str__(self):
         return self.marca
+
+class NombreCerveza(models.Model):
+    tipo=models.OneToOneField(TipoCerveza, null=True, on_delete=models.CASCADE)
+    #marca=models.OneToOneField(MarcaCerveza, on_delete=models.CASCADE)
+    nombre=models.CharField(max_length=30, blank=False, null= False)
+    color=models.CharField(max_length=30, blank=False, null= False)
+    descripcion=models.CharField(max_length=300)
+
+    class Meta:
+        ordering=["tipo"]
+        verbose_name= 'cerveza'
+        verbose_name_plural = "cervezas"
+
+    def __str__(self):
+        return "%s %s %s %s" % (self.tipo, self.nombre, self.color, self.descripcion)
